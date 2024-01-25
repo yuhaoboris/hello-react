70de6042803092df561e19a22e0619be892b97c9
boris <yuhaoboris@gmail.com>
2024-01-25 21:58:37 +0800

    Section 3: 46. storing components in files

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,6 @@
-/* eslint-disable react/prop-types */
-import logo from './assets/react-core-concepts.png'
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
 import { CORE_CONCEPTS } from './data.js'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function genRandomInt (max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-
-  return (
-    <header>
-      <img src={logo} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>{description} React concepts you will need for almost any app you are going to build!</p>
-    </header>
-  )
-}
-
-function CoreConcept ({ title, description, image }) {
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-}
 
 function App() {
   return (
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export default function CoreConcept ({ title, description, image }) {
+  return <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
