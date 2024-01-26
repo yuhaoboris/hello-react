da4dcacd4e04bbc5cc3b29ecfe93658ce12c04f1
boris <yuhaoboris@gmail.com>
2024-01-26 14:44:55 +0800

    Section 3: 57. outputting list data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item, index) => <CoreConcept key={index} {...item } />)}
           </ul>
         </section>
 
