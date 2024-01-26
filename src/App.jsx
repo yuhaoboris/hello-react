cd5c444c3a6f075e35c8c01c75615c845d491d84
boris <yuhaoboris@gmail.com>
2024-01-26 11:33:16 +0800

    Section 3: 54. deriving & outputting data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import CoreConcept from './components/CoreConcept.jsx'
 import { CORE_CONCEPTS } from './data.js'
 import TabButton from './components/TabButton.jsx'
 import { useState } from 'react'
+import { EXAMPLES } from './data.js'
 
 function App() {
-  const [currentTopic, setCurrentTopic] = useState('Please click a button')
+  const [currentTopic, setCurrentTopic] = useState('components')
 
   function handleClick (selectedTopic) {
     setCurrentTopic(selectedTopic)
@@ -35,7 +36,13 @@ function App() {
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
 
-          <div>{currentTopic}</div>
+          {currentTopic && <div id="tab-content">
+            <h3>{EXAMPLES[currentTopic].title}</h3>
+            <p>{EXAMPLES[currentTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[currentTopic].code}</code>
+            </pre>
+          </div>}
         </section>
       </main>
     </div>
